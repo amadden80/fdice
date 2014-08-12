efe1c1cdde5a2ae9026c4b48cfcbd1b74f9c2142
amadden80 <amadden80@gmail.com>
2014-08-11 22:00:26 -0400

    flirt on

---
+++ javascripts/scripts.js
@@ -0,0 +1,23 @@
+
+  function roll(){
+    var bodyParts = ['face', 'lips', 'butt', 'crotch', 'hand', 'foot', 'neck', 'back', 'stomach', 'knee', 'elbow', 'armpit']
+    var actionItems = ['lick', 'kiss', 'bite', 'rub', 'blow on', 'thing about', 'tickle']
+
+    for (var i = 0; i < 1000; i+=100) {
+      setTimeout(function(){
+        $('.action-die').text(actionItems[Math.floor(Math.random()*actionItems.length)])
+        $('.action-die').css({'transform': 'rotate('+ (Math.random()*90-45)+'deg)'})
+        $('.action-die').animate({'left': Math.random()*75+'%', 'top': Math.random()*50+'%'}, 100)
+      }, i * Math.random())
+      setTimeout(function(){
+        $('.body-die').text(bodyParts[Math.floor(Math.random()*bodyParts.length)])
+        $('.body-die').css({'transform': 'rotate('+ (Math.random()*90-45)+'deg)'})
+        $('.body-die').animate({'left': Math.random()*75+'%', 'top': Math.random()*50+'%'}, 100)
+      }, i * Math.random())
+    };
+  }
+
+$(function(){
+  $('body').on('click', roll)
+  roll()
+})
